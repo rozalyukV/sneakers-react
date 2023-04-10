4ae9c9d45c8112977d9f27f1c76f0e7625b9a5cb
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-04-10 19:06:33 +0200

    created scss module for Card component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,15 +1,17 @@
-const Card = () => {
+import styles from './card.module.scss'
+
+const Card = (props) => {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Men's Sneakers Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Price:</span>
-          <b>200 $</b>
+          <b>{props.price} $</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="./img/plus.svg" alt="Plus" />
